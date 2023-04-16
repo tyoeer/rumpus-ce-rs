f38998d82077564194c4da47c79806eb9737c67d
tyoeer <tyoeer@users.noreply.github.com>
2023-04-16 16:23:08 +0200

    Add some top-level documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+/*!
+
+This crate tries to provide an interface to the [Rumpus CE API](https://www.bscotch.net/rumpus-ce) from Butterscotch Shenanigans.
+
+It is build on [restson](https://crates.io/crates/restson), which is build on [hyper](https://crates.io/crates/hyper) and [tokio](https://crates.io/crates/tokio)
+
+See the RestPath implementations for the [types::Rumpus#trait-implementations] type to see which endpoints you can currently use.
+
+*/
+
 ///Typed structs for Rumpus data
 pub mod types;
 ///Stuff to handle parameters we send along
